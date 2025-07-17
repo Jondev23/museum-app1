2262067fbbf7a988ea8dd65121d46cc3e9667ad2
Jondev23 <jonathan@7south.digital>
2025-07-17 21:33:08 +0200

    Respaldo automático 2025-07-17 21:33

--- components/shared/LanguageSelectorIcon.js
+++ components/shared/LanguageSelectorIcon.js
@@ -22,7 +22,7 @@ const LanguageSelectorIcon = ({
   const getContainerStyles = () => {
     return {
       flexShrink: 0,
-      marginBottom: 'min(5.625rem, 9vh)',
+      marginBottom: 'min(6rem, 9.5vh)',
       marginLeft: 'min(5.125rem, 8vw)',
       ...style
     };
