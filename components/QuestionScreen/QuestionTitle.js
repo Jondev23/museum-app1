2defb8ea91e2ed3fb7eafdf7db7efb7728a2045f
Jondev23 <jonathan@7south.digital>
2025-08-04 15:59:00 +0200

    Respaldo automático 2025-08-04 15:59

--- components/QuestionScreen/QuestionTitle.js
+++ components/QuestionScreen/QuestionTitle.js
@@ -4,7 +4,7 @@ import { processTextWithHTML } from '../../utils/textProcessor';
 const QuestionTitle = ({ question }) => {
   return (
     <h1
-      className="typography-fragen relative w-full overflow-hidden flex items-center justify-center text-center mb-[min(3rem,4vh)] box-border !px-0"
+      className="typography-fragen relative w-full overflow-hidden flex items-center justify-center text-center mb-[min(3rem,4vh)] box-border px-[15rem]"
     >
       {processTextWithHTML(question.question)}
     </h1>
